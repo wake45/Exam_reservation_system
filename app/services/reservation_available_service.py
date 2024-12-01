8a2c908b1e4123fe5d0fee636883f2cef9ec07d8
LeeSangUk <wake45@naver.con>
2024-12-01 12:37:38 +0900

    신규 조회시 예약내역 최대 10일까지 보여지도록 수정

--- app/services/reservation_available_service.py
+++ app/services/reservation_available_service.py
@@ -11,17 +11,17 @@ class ReservationAvailableService:
 
     def select_available_reservation(self):
         
-        # 오늘 날짜 기준으로 6일 전과 3일 전 계산
+        # 오늘 날짜 기준으로 10일 전과 3일 전 계산
         today = datetime.now()
-        six_days_ahead = today + timedelta(days=6)
+        ten_days_ahead = today + timedelta(days=10)
         three_days_ahead = today + timedelta(days=3)
 
-        # 오늘 날짜 기준으로 6일전 부터 3일전 확정된 데이터만 조회
+        # 오늘 날짜 기준으로 10일전 부터 3일전 확정된 데이터만 조회
         query = self.db.query(Reservations).filter(
             Reservations.reservation_type == ReservationStatus.CONFIRMED,
             and_(
                 Reservations.exam_start_date >= three_days_ahead,
-                Reservations.exam_start_date <= six_days_ahead
+                Reservations.exam_start_date <= ten_days_ahead
             )
         )
 
